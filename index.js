03955fcb8c28e0894080086b6d7795e75d09f6f9
Léon Meyer - dev <70799685+leon-linetim@users.noreply.github.com>
2020-11-01 11:08:18 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const {Client, MessageEmbed} = require('discord.js'),
+tcpp = require('tcp-ping');
+client = new Client();
+client.login(config.token)
+
+client.on('ready', () => {
+    console.log('ready')
+
+    let addr = config.server,
+    port = config.port;
+    let ping = new MessageEmbed()
+    .setTitle(`Ping`)
+    .setColor(`ORANGE`)
+    .addField(addr, `pinging`)
+    client.channels.resolve(config.channel).send(ping)
+    .then(msg => {
+        setInterval(() => {
+            tcpp.probe(addr, port, function(err, available) {
+                if (available) {
+                    tcpp.ping({ address: addr, port: port }, function(err, data) {
+                        console.log(data);
+                        let ping = new MessageEmbed()
+                        .setTitle(`NODE JEUX`)
+                        .setColor(`GREEN`)
+                        .addField(Math.floor(data.avg) + `ms`, `Le serveur est actuellement en ligne`)
+                        .setFooter(`Dernière actualisation ${new Date().toLocaleString("fr-FR", {timeZone: "Europe/Paris"}).split(',')[0]} | powered by Linetim.fr`)
+                        msg.edit(ping)
+                    });
+                } else {
+                    let ping = new MessageEmbed()
+                    .setTitle(`NODE JEUX`)
+                    .setColor(`RED`)
+                    .addField(`->`, `down`)
+                    .setFooter(`Dernière actualisation ${new Date().toLocaleString("fr-FR", {timeZone: "Europe/Paris"}).split(',')[0]} | powered by Linetim.fr`)
+                    msg.edit(ping)
+                }
+            })
+        }, 5000)
+    })
+})
+
